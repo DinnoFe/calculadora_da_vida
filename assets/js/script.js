068f6b5a71a0e0fc635d28a129ed2aa6c6d0dffd
Felipe França <0double.xp0@gmail.com>
2026-01-01 17:25:37 -0300

    Refeito a funcao de calculadora_da_vida e criacao de variaveis para os valores exibidos em tela + ajustes de design basicos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
+const dias_vivo = document.getElementById("dias_vivo");
+const horas_vivo = document.getElementById("horas");
+const minutos_vivo = document.getElementById("minutos");
+
+dias_vivo.innerText = 0;
+horas_vivo.innerText = 0;
+minutos_vivo.innerText = 0; 
+
 function calculadora_da_vida() {
     const userIdade = Number(document.getElementById("idade").value);
-    
     const umAno = 365;
-    const tempoDeVida = userIdade * umAno;
+    const umDia = 24;
+    const umaHora = 60;
 
-    document.getElementById("tempoDeVida").innerText = ` ${tempoDeVida} `;
-    
-    return tempoDeVida;
+    const tempoDeVida_dias = userIdade * umAno;
+    const tempoDeVida_horas = tempoDeVida_dias * umDia;
+    const tempoDeVida_minutos = tempoDeVida_horas * umaHora;
+
+    dias_vivo.innerText = tempoDeVida_dias.toLocaleString('pt-BR');
+    horas_vivo.innerText = tempoDeVida_horas.toLocaleString('pt-BR');
+    minutos_vivo.innerText = tempoDeVida_minutos.toLocaleString('pt-BR');
 }
+
